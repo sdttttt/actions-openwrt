d5b94e5ee775743b35511460945d3410b7dbae42
Cow <42728902+sdttttt@users.noreply.github.com>
2023-12-13 17:09:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,3 +31,6 @@ sed -i 's,-mcpu=generic,-march=armv8-a,g' include/target.mk
 # 交换 LAN/WAN 口
 #sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 #sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+# luci-app-daed-next
+git clone https://github.com/sbwml/luci-app-daed-next package/daed-next
