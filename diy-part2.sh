3fe1bf95cbe35da8e160cc64f8e8f1ec762d8b2d
sdttttt <42728902+sdttttt@users.noreply.github.com>
2023-12-21 20:19:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,12 +16,12 @@
 # luci-app-mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns --depth=1  -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata --depth=1  package/v2ray-geodata
 
 # luci-app-openclash
 find ./ | grep Makefile | grep openclash | xargs rm -f
-git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash --depth=1  package/luci-app-openclash
 
 # luci-app-daed-next
 # git clone https://github.com/sbwml/luci-app-daed-next package/daed-next
