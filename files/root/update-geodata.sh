986c0dff50b0f9482f3fb5294cc5cb286236f270
sdttttt <42728902+sdttttt@users.noreply.github.com>
2023-12-28 13:08:36 +0800

    Create update-geodata.sh

---
+++ files/root/update-geodata.sh
@@ -0,0 +1,6 @@
+curl -o /usr/share/daed/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+
+curl -o /usr/share/daed/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+
+
+--logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1
