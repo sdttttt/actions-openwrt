d42cbe4ffbca5880042a71d957b50981d1ea3f14
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-01-24 12:44:53 +0800

    Update update-geodata.sh

--- files/root/update-geodata.sh
+++ files/root/update-geodata.sh
@@ -1,6 +1,6 @@
-curl -o /usr/share/daed/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+curl -o /usr/share/dae/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
 
-curl -o /usr/share/daed/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+curl -o /usr/share/dae/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
 
 
---logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1
+# --logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1
