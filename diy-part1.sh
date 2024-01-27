829e7d6faf52f2443ffaf473c715d5f4bdd09be7
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-01-27 16:41:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 使用 O3 级别的优化
-sed -i 's/Os/O3/g' include/target.mk
-
-# 使用专属优化
-sed -i 's,-mcpu=generic,-march=armv8-a,g' include/target.mk
-
 # 交换 LAN/WAN 口
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
