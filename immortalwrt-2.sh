e834bddb41cf5bbb44c7107168b0519b13192479
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-03-30 15:40:27 +0800

    Update immortalwrt-2.sh

--- immortalwrt-2.sh
+++ immortalwrt-2.sh
@@ -13,7 +13,8 @@
 set +e
 
 # luci-app-openclash
+find ./ | grep Makefile | grep openclash | xargs rm -f  
 git clone https://github.com/vernesong/OpenClash --depth=1  package/luci-app-openclash 
 
 # 修改 dae.init 加上日志记录
-[ -e package/feeds/packages/dae/files/dae.init ] && sed -i 's,run -c "$config_file",run -c "$config_file" --logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1,g' package/feeds/packages/dae/files/dae.init
+# [ -e package/feeds/packages/dae/files/dae.init ] && sed -i 's,run -c "$config_file",run -c "$config_file" --logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1,g' package/feeds/packages/dae/files/dae.init
