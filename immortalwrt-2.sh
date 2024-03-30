03a7f6244bf1850dfbf8ccf22e1156b333b5bb3c
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-03-30 15:47:56 +0800

    Update immortalwrt-2.sh

--- immortalwrt-2.sh
+++ immortalwrt-2.sh
@@ -12,9 +12,18 @@
 
 set +e
 
+# luci-app-mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns --depth=1  -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata --depth=1  package/v2ray-geodata
+
 # luci-app-openclash
 find ./ | grep Makefile | grep openclash | xargs rm -f  
 git clone https://github.com/vernesong/OpenClash --depth=1  package/luci-app-openclash 
 
+# luci-app-daed-next
+git clone https://github.com/sbwml/luci-app-daed-next package/daed-next
+
 # 修改 dae.init 加上日志记录
-# [ -e package/feeds/packages/dae/files/dae.init ] && sed -i 's,run -c "$config_file",run -c "$config_file" --logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1,g' package/feeds/packages/dae/files/dae.init
+[ -e package/feeds/packages/dae/files/dae.init ] && sed -i 's,run -c "$config_file",run -c "$config_file" --logfile /var/log/dae.log --logfile-maxbackups 1 --logfile-maxsize 1,g' package/feeds/packages/dae/files/dae.init
