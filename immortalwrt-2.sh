f6979e24742a686e8d394323b2f48fc1263158e2
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-03-30 15:24:36 +0800

    Update immortalwrt-2.sh

--- immortalwrt-2.sh
+++ immortalwrt-2.sh
@@ -12,12 +12,6 @@
 
 set +e
 
-# luci-app-mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns --depth=1  -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata --depth=1  package/v2ray-geodata
-
 # luci-app-openclash
 find ./ | grep Makefile | grep openclash | xargs rm -f  
 git clone https://github.com/vernesong/OpenClash --depth=1  package/luci-app-openclash 
