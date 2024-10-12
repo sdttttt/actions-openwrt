2fd45fb7a3b6bde0ed0a4b148f8d51a42c2da607
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-10-12 22:19:26 +0800

    Update immortalwrt-1.sh

--- immortalwrt-1.sh
+++ immortalwrt-1.sh
@@ -23,9 +23,23 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 # 添加DAE所需要的geodata
-if [ -e /workdir/files ]
-then
-  mkdir /workdir/files/usr/share/dae
-  curl -o /workdir/files/usr/share/dae/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
-  curl -o /workdir/files/usr/share/dae/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
-fi
+#if [ -e /workdir/files ]
+#then
+#  mkdir /workdir/files/usr/share/dae
+#  curl -o /workdir/files/usr/share/dae/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+#  curl -o /workdir/files/usr/share/dae/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+#fi
+
+echo "克隆 YAOF"
+git clone -b 23.05 --depth 1 https://github.com/QiuSimons/luci-app-daed /workdir/.YAOF
+
+echo "打打补丁~"
+### 必要的 Patches FROM YAOF ###
+# TCP optimizations
+cp -rf /workdir/.YAOF/PATCH/backport/TCP/* ./target/linux/generic/backport-5.15/
+# x86_csum
+# cp -rf /workdir/.YAOF/PATCH/backport/x86_csum/* ./target/linux/generic/backport-5.15/
+# BBRv3
+cp -rf /workdir/.YAOF/PATCH/BBRv3/kernel/* ./target/linux/generic/backport-5.15/
+# LRNG
+# cp -rf ../PATCH/LRNG/* ./target/linux/generic/hack-5.15/
